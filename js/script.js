c86811c13c71c0acc841400b9bcb8a5874b6cc2d
Damiano <ghgg585@gmail.com>
2024-03-07 16:51:23 +0100

    Feat :add bonus

--- js/script.js
+++ js/script.js
@@ -3,30 +3,35 @@
 // Stampare in console i numeri da 1 a 100
 for (let i= 1; i <= 100; i++){
     let fuzzOrPuzz = i;
-    
+    const box = document.createElement('div');
+    const container = document.querySelector('#container');
+    let classFuzzOrPuzz;
     //fare in modo che i multipli di 3 e 5 stampino FizzBuzz
     if (i % 3 === 0 & i % 5 ===0) {
         fuzzOrPuzz = 'FuzzPuzz';
+        classFuzzOrPuzz = 'fuzz-puzz'
+        box.classList.add(classFuzzOrPuzz)
         console.log(fuzzOrPuzz);
+
     //fare in modo che i multipli di 5 stampi buzz
     } else if(i % 5 === 0){
         fuzzOrPuzz = 'Puzz';
-
+        classFuzzOrPuzz = 'puzz'
+        box.classList.add(classFuzzOrPuzz)
         console.log(fuzzOrPuzz);
         
         //fare in modo che i multipli di 3 stampi fuzz
     } else if (i % 3 === 0) {
         fuzzOrPuzz = 'Fuzz';
-        
+        classFuzzOrPuzz = 'fuzz'
+        box.classList.add(classFuzzOrPuzz)
         console.log(fuzzOrPuzz);
     }
     else{
-    console.log(i);
-
+        console.log(i);
+        
     }
-    const box = document.createElement('div');
     box.innerHTML = fuzzOrPuzz;
-    const container = document.querySelector('#container');
     container.append(box);
-    
+    box.classList.add('box')
 }
