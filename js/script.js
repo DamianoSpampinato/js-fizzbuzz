1742455aa1fd12c80b281ee4b4045743b052e390
Damiano <ghgg585@gmail.com>
2024-03-07 15:48:56 +0100

    Feat : add script

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 // Stampare in console i numeri da 1 a 100
-//fare in modo che i multipli di 3 e 5 stampino FizzBuzz
-//fare in modo che i multipli di 5 stampi buzz
-//fare in modo che i multipli di 3 stampi fuzz
+for (let i= 1; i <= 100; i++){
+    console.log(i);
+    //fare in modo che i multipli di 3 e 5 stampino FizzBuzz
+    if (i % 3 === 0 & i % 5 ===0) {
+        console.log(' FuzzPuzz');
+    //fare in modo che i multipli di 5 stampi buzz
+    } else if(i % 5 === 0){
+        console.log(' Puzz');
+        
+        //fare in modo che i multipli di 3 stampi fuzz
+    } else if (i % 3 === 0) {
+        console.log(' Fuzz');
+    }
+}
