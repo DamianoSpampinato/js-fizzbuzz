843147379581a70062d64ffd1c8fb3ffa6b1445b
Damiano <ghgg585@gmail.com>
2024-03-07 15:23:50 +0100

    Feat: add workflow comments

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Stampare in console i numeri da 1 a 100
+//fare in modo che i multipli di 3 e 5 stampino FizzBuzz
+//fare in modo che i multipli di 5 stampi buzz
+//fare in modo che i multipli di 3 stampi fuzz
