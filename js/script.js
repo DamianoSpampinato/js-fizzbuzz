32443b55bfcd0312307ee87ce92b7af10526c0ab
Damiano <ghgg585@gmail.com>
2024-03-07 16:06:56 +0100

    fix script

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
+
+let fuzzOrPuzz;
+
 // Stampare in console i numeri da 1 a 100
 for (let i= 1; i <= 100; i++){
-    console.log(i);
     //fare in modo che i multipli di 3 e 5 stampino FizzBuzz
     if (i % 3 === 0 & i % 5 ===0) {
-        console.log(' FuzzPuzz');
+        fuzzOrPuzz = 'FuzzPuzz';
+        console.log(fuzzOrPuzz);
     //fare in modo che i multipli di 5 stampi buzz
     } else if(i % 5 === 0){
-        console.log(' Puzz');
+        fuzzOrPuzz = 'Puzz';
+
+        console.log(fuzzOrPuzz);
         
         //fare in modo che i multipli di 3 stampi fuzz
     } else if (i % 3 === 0) {
-        console.log(' Fuzz');
+        fuzzOrPuzz = 'Fuzz';
+        
+        console.log(fuzzOrPuzz);
+    }
+    else{
+    console.log(i);
+
     }
 }
